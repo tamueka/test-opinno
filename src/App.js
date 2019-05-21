52dac38e822bdfc0fa7afb58a6d558eb4d95cb9c
tamueka <tamueka@hotmail.com>
2019-05-22 00:05:06 +0200

    create component filmcard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="StarWars">
-      <h1>STAR WARS</h1>
-    </div>
-  );
-}
+export class FilmCard extends Component {
+  render (){
+    return(
+        <div className="filmCard">
+          <App />
+          <h2>Nombre pelicula</h2>
+          <p>Director</p>
+          <p>Episodio</p>
+          <p>Productor</p>
+        </div>
+        );
+      }
+  }
 
-export default App;
+export class App extends Component {
+  render (){
+    return(
+        <div className="StarWars">
+          <h1>STAR WARS</h1> 
+        </div>
+        );
+      }
+  }
+
+export default FilmCard;
