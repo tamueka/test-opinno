baac40106c7f42b9b06dacd269ce40cc5aef4c31
tamueka <tamueka@hotmail.com>
2019-05-23 01:33:57 +0200

    fix errors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import FilmsCard from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render( <FilmsCard / > , document.getElementById('root'));
+ReactDOM.render( <FilmsCard /> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
