a33b00dabe08038d7176166a530502c128dbb3a2
tamueka <tamueka@hotmail.com>
2019-05-23 23:52:59 +0200

    add people.json to Data folder

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import FilmsCard from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render( <FilmsCard /> , document.getElementById('root'));
+ReactDOM.render( <FilmsCard/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
